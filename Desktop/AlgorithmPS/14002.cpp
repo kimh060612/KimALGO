15f1806e68510801a9b32547691cf6a23fc0f8e5
kimh060612 <kimh060612@naver.com>
2021-06-11 21:54:47 +0900

    .

--- Desktop/AlgorithmPS/14002.cpp
+++ Desktop/AlgorithmPS/14002.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
 #include <vector>
-#define MAX 1000001
+#include <algorithm>
 
 using namespace std;
 
-int N;
-vector<int> A;
+int N, A[1001], DP[2][1001];
+int ans, ans_i;
+vector<int> K;
 
 int main()
 {
     cin >> N;
-    int sub;
-    cin >> sub;
-    A.push_back(sub);
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> A[i];
+    }
 
-    for (int i = 1; i < N; i++) {
-        int sub;
-        cin >> sub;
-
-        int size = A.size();
-        for (int j = size - 1; j >= 0; j --) {
-            if (A.at(j) < sub) {
-                if (j == size - 1)A.push_back(sub);
-                else A.at(j + 1) = sub;
-                break;
+    for (int i = 1; i <= N; i++)
+    {
+        DP[0][i] = -1;
+        DP[1][i] = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            if (A[i] > A[j] && DP[1][i] < DP[1][j] + 1)
+            {
+                DP[1][i] = DP[1][j] + 1;
+                DP[0][i] = j;
             }
-            if (j == 0)A.at(0) = sub;
         }
     }
 
-    cout << A.size() << endl;
+    for (int i = 1; i <= N; i++)
+    {
+        if (ans < DP[1][i])
+        {
+            ans = DP[1][i];
+            ans_i = i;
+        }
+    }
 
+    int t = ans_i;
+    while (t != -1)
+    {
+        K.push_back(A[t]);
+        t = DP[0][t];
+    }
+
+    reverse(K.begin(), K.end());
+    cout << ans << endl;
+    for (int i = 0; i < K.size(); i++)
+    {
+        cout << K[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
