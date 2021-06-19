cc131e61efad6ba4cbebc389145e0d638bfc7406
kimh060612 <kimh060612@naver.com>
2021-06-19 15:17:10 +0900

    .

---
+++ Desktop/AlgorithmPS/1074.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N, r, c;
+
+int Zfunc(int n, int y, int x)
+{
+    if (n == 2) return 1;
+
+    
+
+}
+
+int main()
+{
+    cin >> N >> r >> c;
+
+
+
+    return 0;
+}
