d1cd5f6e88ada355289459bd78de0be2970d908b
Michael <kimh060612@khu.ac.kr>
2021-08-16 16:33:53 +0900

    .

---
+++ Desktop/AlgorithmPS/10156.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, K, M;
+    cin >> N >> K >> M;
+    int ans = (N * K - M) > 0 ? (N * K - M) : 0;
+    cout << ans << endl; 
+    return 0;
+}
