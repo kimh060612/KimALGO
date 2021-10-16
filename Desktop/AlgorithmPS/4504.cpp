d16cd335287beeb23ae01194fac8ec61d41e085f
kimh060612 <kimh060612@naver.com>
2021-10-16 16:44:31 +0900

    .

---
+++ Desktop/AlgorithmPS/4504.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (1)
+    {
+        int a;
+        cin >> a;
+        if (a == 0) break;
+        if (a % n == 0) cout << a << " is a multiple of " << n << ".\n";
+        else cout << a << " is NOT a multiple of " << n << ".\n";
+    }
+    return 0;
+}
