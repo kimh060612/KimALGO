04c26124aee81e1bcebb4b7bcd07b93d54f98a59
Michael <kimh060612@autocrypt.io>
2022-12-30 12:11:43 +0900

    .

--- Desktop/AlgorithmPS/13977.cpp
+++ Desktop/AlgorithmPS/13977.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 using ll = long long;
 
+ll fact[4000001];
+
 ll fpow(ll a, ll b) {
     ll res = 1;
     while(b) {
         if (b & 1) {
-            res *= a;
+            res = (res * a) % MOD;
         }
-        a *= a;
+        a = (a * a) % MOD;
         b /= 2;
     }
     return res;
@@ -19,11 +21,20 @@ ll fpow(ll a, ll b) {
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int m, n, k;
     cin >> m;
+    fact[0] = 1;
+    for (int i = 1; i <= 4000000; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+    }
     for (int i = 1; i <= m; i++) {
-        cin >> m;
-
+        cin >> n >> k;
+        ll tmp = (fact[k] * fact[n - k]) % MOD;
+        ll ans = (fact[n] * fpow(tmp, MOD - 2)) % MOD;
+        cout << ans << '\n';
     }
     return 0;
 }
