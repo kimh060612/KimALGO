df056ac26ae14cf4738cd347c0b22fbb9ad1247f
Michael <kimh060612@autocrypt.io>
2023-01-02 18:07:49 +0900

    .

--- Desktop/AlgorithmPS/16287.cpp
+++ Desktop/AlgorithmPS/16287.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string.h>
 #include <vector>
-#include <map>
+#include <algorithm>
 #define MAXW 799994
 
 using namespace std;
 
-int w, n, A[5001], DP[MAXW];
+int w, n, A[5001], DP[MAXW], check[MAXW];
 
 int main()
 {
@@ -16,14 +16,21 @@ int main()
     cin >> w >> n;
     for (int i = 1;i <= n; i++) {
         cin >> A[i];
+        check[A[i]] = 1;
     }
     bool ans = false;
+    sort(A + 1, A + 1 + n);
 
     for(int i = 1; i <= n; i++) 
     {
         for (int j = i + 1; j <= n; j++) {
-            
             DP[A[i] + A[j]] = 1;
+            if (w - (A[i] + A[j]) >= 0 && DP[w - (A[i] + A[j])]) {
+                int tmp = w - (A[i] + A[j]);
+                if (tmp - A[i] < 0 || tmp - A[j] < 0) continue;
+                int k = tmp - A[i];
+                if (check[k] || check[A[i]]) DP[A[i] + A[j]] = 0;
+            }
         }
     }
 
