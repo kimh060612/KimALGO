265b20ce748c55e13dd9368ecbaee7e54f3a116a
michael <kimh060612@naver.com>
2023-01-03 00:55:17 +0900

    .

--- Desktop/AlgorithmPS/16287.cpp
+++ Desktop/AlgorithmPS/16287.cpp
@@ -24,20 +24,19 @@ int main()
     for(int i = 1; i <= n; i++) 
     {
         for (int j = i + 1; j <= n; j++) {
-            DP[A[i] + A[j]] = 1;
+            DP[A[i] + A[j]] += 1; // 7 (5 + 2) & 3 (2 + 1)
             if (w - (A[i] + A[j]) >= 0 && DP[w - (A[i] + A[j])]) {
-                int tmp = w - (A[i] + A[j]);
-                if (tmp - A[i] < 0 || tmp - A[j] < 0) continue;
-                int k = tmp - A[i];
-                if (check[k] || check[A[i]]) DP[A[i] + A[j]] = 0;
+                
             }
         }
     }
 
-    for (int i = 10; i <= w; i++) 
+    for (int i = 1; i < w; i++) 
     {
-        if (DP[i] && DP[w - i]) ans = true;
+        cout << DP[i] << " ";
+        if (DP[i] && DP[w - i] && w != 2 * i) ans = true;
     }
+    cout << endl;
 
     if (ans) cout << "YES" << endl;
     else cout << "NO" << endl;
