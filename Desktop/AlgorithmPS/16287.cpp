9dac6840fe6c2264575f02b90f6c1e086df54d00
Michael <kimh060612@autocrypt.io>
2023-01-03 13:24:43 +0900

    .

--- Desktop/AlgorithmPS/16287.cpp
+++ Desktop/AlgorithmPS/16287.cpp
@@ -5,8 +5,10 @@
 #define MAXW 799994
 
 using namespace std;
+using iip = pair<int, int>;
 
 int w, n, A[5001], DP[MAXW], check[MAXW];
+vector<iip> Cache;
 
 int main()
 {
@@ -16,27 +18,34 @@ int main()
     cin >> w >> n;
     for (int i = 1;i <= n; i++) {
         cin >> A[i];
-        check[A[i]] = 1;
+        check[A[i]] += 1;
     }
     bool ans = false;
     sort(A + 1, A + 1 + n);
 
     for(int i = 1; i <= n; i++) 
-    {
+    { // w == 10
         for (int j = i + 1; j <= n; j++) {
-            DP[A[i] + A[j]] += 1; // 7 (5 + 2) & 3 (2 + 1)
-            if (w - (A[i] + A[j]) >= 0 && DP[w - (A[i] + A[j])]) {
-                
+            DP[A[i] + A[j]] += 1;
+        }
+    }
+    // for (int i = 1; i <= w; i++) {
+    //     cout << "(" << i << "," << DP[i] << ")" << " ";
+    // }
+    // cout << endl;
+
+    for(int i = 1; i <= n; i++) 
+    { // w == 21
+        for (int j = i + 1; j <= n; j++) {
+            int now = A[i] + A[j]; // 6
+            if (w - now >= 0 && DP[w - now]) {
+                int pair_w = w - now; // 15
+                int other_i = pair_w - A[i]; // 1(A[i]), 14(other_i)
+                int other_j = pair_w - A[j]; // 5(A[j]), 10(other_j)
+                if ((other_i > 0 && other_j > 0) && !check[other_i] && !check[other_j]) ans = true;
             }
         }
     }
-
-    for (int i = 1; i < w; i++) 
-    {
-        cout << DP[i] << " ";
-        if (DP[i] && DP[w - i] && w != 2 * i) ans = true;
-    }
-    cout << endl;
 
     if (ans) cout << "YES" << endl;
     else cout << "NO" << endl;
