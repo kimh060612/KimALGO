51c5721115868a11800de28684838c16764d4d5e
michael <kimh060612@naver.com>
2023-01-04 23:28:49 +0900

    .

--- Desktop/AlgorithmPS/13710.cpp
+++ Desktop/AlgorithmPS/13710.cpp
@@ -4,33 +4,25 @@ using namespace std;
 typedef long long ll;
 
 int N; 
-ll A[100001], DP[100001], Sum[100001];
-int bit[100];
+ll A[100001], XOR[100001];
+
+int step(ll k) { return k > 0 ? 1 : 0; }
 
 int main()
 {
     cin >> N;
     for (int i = 1; i <= N; i++) cin >> A[i];
-    
-    DP[1] = A[1];
-    Sum[1] = A[1];
-    for (int i = 2; i <= N; i++)
-    {
-        ll ans = 0;
-        DP[i] = A[i];
-        for (int j = i - 1; j >= 1; j--)
-        {
-            DP[j] = DP[j] ^ A[i];
-            ans += DP[j];
-        }
-        ans += A[i];
-        Sum[i] = ans;
-    }
+    for (int i = 1; i <= N; i++) XOR[i] = A[i] ^ XOR[i - 1];
 
     ll ans = 0;
-    for (int i = 1; i <= N; i++) ans += Sum[i];
+    int bits[31] = {0, };
+    for (int i = 0; i < 30; i++) {
+        for (int j = 0; j <= N; j++) {
+            if (XOR[j] & (1 << i)) bits[i]++;
+        }
+    }
 
+    for (int i = 0; i < 30; i++) ans += ((ll)1 << i) * bits[i] * (N + 1 - bits[i]);
     cout << ans << endl;
-
     return 0;
 }
