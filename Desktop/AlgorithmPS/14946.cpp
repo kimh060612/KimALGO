2a9aa2d038ae6bf776ef90e2f0aa6a97de0f8226
Michael <kimh060612@autocrypt.io>
2023-01-18 12:52:14 +0900

    .

--- Desktop/AlgorithmPS/14946.cpp
+++ Desktop/AlgorithmPS/14946.cpp
@@ -9,14 +9,14 @@ using iip = pair<int, int>;
 int V, Q, K, check[1001];
 vector<int> T[1001];
 int Cost[1001][5]; // 1: R, 2: G, 3: B
-int DP[1001][5][10001]; // i번째 Node가 j-color이고 여기까지의 비용합이 k일때 이를 루트로 하는 서브트리가 취할 수 있는 경우의 수 
+int DP[1001][5][20]; // i번째 Node가 j-color이고 여기까지의 비용합이 k일때 이를 루트로 하는 서브트리가 취할 수 있는 경우의 수 
 // K로 나누어 떨어질 수 있어야함.
 // G - G 연결은 불가.
 // B는 G끼리만 연결 가능.
 
 int dfs(int now, int ncolor, int nowCost, int parent) {
-    if (T[now].size() == 0) return DP[now][ncolor][Cost[now][ncolor]] = Cost[now][ncolor];
-    int &ret = DP[now][ncolor][nowCost];
+    if (T[now].size() == 0) return DP[now][ncolor][Cost[now][ncolor] % K] = Cost[now][ncolor];
+    int &ret = DP[now][ncolor][nowCost % K];
     if (ret != -1) return ret;
 
     ret = 0;
@@ -26,7 +26,8 @@ int dfs(int now, int ncolor, int nowCost, int parent) {
         for (int j = 1; j <= 3; j++) {
             if (ncolor == 3 && j == 3) continue;
             if (ncolor == 2 && j != 3) continue;
-
+            int nextCost = nowCost + Cost[next][j];
+            
         }
     }
 
