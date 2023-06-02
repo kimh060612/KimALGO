a6e1ad0c51193927bab9f9403cc1ebd3bb52aea3
Michael <kimh060612@autocrypt.io>
2023-06-02 18:10:52 +0900

    .

---
+++ Desktop/AlgorithmPS/15480.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+vector<int> T[100001];
+int parent[100001], depth[100001];
+
+int dfs(int x, int parent) {
+    
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    for (int i = 1; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        T[a].push_back(b);
+        T[b].push_back(a);
+    }
+
+    while (M--) {
+        int r, u, v;
+
+    }
+
+    return 0;
+}
