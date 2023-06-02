af619b55ae099238a9f409554e5ad8eed7bd352a
Michael <kimh060612@autocrypt.io>
2023-06-02 11:17:17 +0900

    .

--- Desktop/AlgorithmPS/1017.cpp
+++ Desktop/AlgorithmPS/1017.cpp
@@ -4,23 +4,30 @@
 #include <algorithm>
 
 using namespace std;
-int N, L[51], Adj[51][51];
-int isPrime[3001], matched[51];
+int N, n, L[51], Adj[51][51];
+int eMatch[51], oMatch[51], visited[51];
+int isPrime[3001];
+vector<int> even, odd;
 
 void Field() {
+    memset(isPrime, 1, sizeof(isPrime));
+    isPrime[0] = 0;
+    isPrime[1] = 0;
+
     for (int i = 2; i * i <= 3000; i++) {
-        if (isPrime[i]) continue;
-        for (int j = 2 * i; j <= 3000; j += i) isPrime[j] = 1;
+        if (!isPrime[i]) continue;
+        for (int j = 2 * i; j <= 3000; j += i) isPrime[j] = 0;
     }
 }
 
 bool dfs(int x) {
-    if (matched[x]) return false;
-    for (int i = 1; i <= N; i++) {
+    if (visited[x]) return false;
+    visited[x] = 1;
+    for (int i = 0; i < n; i++) {
         if (Adj[x][i]) {
-            if (!matched[i] || dfs(matched[i])) {
-                matched[i] = x;
-                matched[x] = i;
+            if (oMatch[i] == -1 || dfs(oMatch[i])) {
+                eMatch[x] = i;
+                oMatch[i] = x;
                 return true;
             }
         }
@@ -28,10 +35,12 @@ bool dfs(int x) {
     return false;
 }
 
-int bipartiteMatch() {
+int biparititeMatching(int novisit) {
     int size = 0;
-    for (int i = 2; i <= N; i++) {
-        if (dfs(i)) ++size;
+    for (int i = 0; i < n; i++) {
+        memset(visited, 0, sizeof(visited));
+        visited[novisit] = 1;
+        if (dfs(i)) size ++;
     }
     return size;
 }
@@ -43,26 +52,43 @@ int main()
     cout.tie(0);
 
     cin >> N;
-    for (int i = 1; i <= N; i++) cin >> L[i];
+    for (int i = 1; i <= N; i++) {
+        cin >> L[i];
+        if (L[i] % 2 == 0) even.push_back(i);
+        else odd.push_back(i);
+    }
+    if (even.size() != odd.size()) {
+        cout << -1 << "\n";
+        return 0;
+    }
     Field();
     int s = L[1];
+    n = N / 2;
     vector<int> P;
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = i; j <= N; j++) {
-            if (i == j) continue;
-            if (isPrime[L[i] + L[j]]) Adj[i][j] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (isPrime[L[even[i]] + L[odd[j]]]) Adj[i][j] = 1;
         }
     }
 
-    for (int j = 2; j <= N; j++) {
-        if (Adj[1][j]) {
-            memset(matched, 0, sizeof(matched));
-            matched[1] = j;
-            matched[j] = 1;
-            int res = bipartiteMatch();
-            cout << res << "\n";
-            if (res == N / 2) P.push_back(L[j]);
+    for (int i = 0; i < n; i++) {
+        memset(eMatch, -1, sizeof(eMatch));
+        memset(oMatch, -1, sizeof(oMatch));
+        if (s == L[even[0]]) {
+            if (Adj[0][i]) {
+                eMatch[0] = i;
+                oMatch[i] = 0;
+                int res = biparititeMatching(0);
+                if (res + 1 == n) P.push_back(L[odd[i]]);
+            }
+        } else if (s == L[odd[0]]){
+            if (Adj[i][0]) {
+                eMatch[i] = 0;
+                oMatch[0] = i;
+                int res = biparititeMatching(i);
+                if (res + 1 == n) P.push_back(L[even[i]]);
+            }
         }
     }
 
