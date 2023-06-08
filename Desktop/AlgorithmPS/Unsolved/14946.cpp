6bc3f4b8240583445eaf5cb71b5be929f042ba9f
Michael <kimh060612@autocrypt.io>
2023-06-08 18:28:07 +0900

    .

--- Desktop/AlgorithmPS/Unsolved/14946.cpp
+++ Desktop/AlgorithmPS/Unsolved/14946.cpp
@@ -1,82 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
-#include <fstream>
+#include <queue>
 
 using namespace std;
-
-ifstream fin("input.txt");
+using iip = pair<int, int>;
 
 int V, Q, K;
 vector<int> G[1001];
 vector<int> T[1001];
 int Cost[1001][5];
-int tDP[1001][20][5][5];
-int check[1001]; // tDP의 업데이트가 끝난 Node.
+int tDP[1001][5][15];
+int tMem[1001][5];
 
-void search(int depth, int ncolor, int nowCost, int parent, int pcolor, int acolor, int factor) {
-    if (T[parent].size() == depth) {
-        tDP[parent][nowCost % K][pcolor][acolor] = (tDP[parent][nowCost % K][pcolor][acolor] + factor) % Q;
-        return;
-    }
-
-    int next = T[parent][depth]; // parent의 자식들중 나머지가 k인 가능성을 쫒아서 간다.
-    for (int i = 1; i <= 3; i++) {
-        if (pcolor == 2 && i == 2) continue;
-        if (pcolor == 3 && i != 2) continue;
-        for (int k = 0; k < K; k++) {
-            if (tDP[next][k][i][pcolor] != 0) {
-                search(depth + 1, i, (nowCost + k) % K, parent, pcolor, acolor, (factor * tDP[next][k][i][pcolor]) % Q);
-            }
-        }
-    }
-}
-
-void dfs(int now, int ncolor, int pcolor) {
-    if (T[now].size() == 0) {
-        int m = Cost[now][ncolor] % K;
-        tDP[now][m][ncolor][pcolor] = 1;
-        return;
-    }
-
-    for (int i = 0; i < T[now].size(); i++) {
-        int next = T[now][i];
-        for (int j = 1; j <= 3; j++) {
-            if (ncolor == 2 && j == 2) continue;
-            if (ncolor == 3 && j != 2) continue;
-            dfs(next, j, ncolor);
-        }
-        check[next] = 1; // 모든 색에 대해서 탐색이 끝나고 나면, 위의 노드에서 다시 탐색할 필요가 없음.
-    }
-
-    if (check[now]) return;
-    for (int i = 1; i <= 3; i++) {
-        if (ncolor == 2 && i == 2) continue;
-        if (ncolor == 3 && i != 2) continue;
-        int next = T[now][0];
-        for (int k = 0; k < K; k++) {
-            if (tDP[next][k][i][ncolor] != 0) {
-                int nextCost = (Cost[now][ncolor] + k) % K;
-                search(1, i, nextCost, now, ncolor, pcolor, tDP[next][k][i][ncolor] % Q);
-            }
-        }
-    }
-    // cout << "R: ";
-    // for (int k = 0; k < K; k++) {
-    //     cout << tDP[now][k][1] << " ";
-    // }
-    // cout << endl;
-    // cout << "G: ";
-    // for (int k = 0; k < K; k++) {
-    //     cout << tDP[now][k][2] << " ";
-    // }
-    // cout << endl;
-    // cout << "B: ";
-    // for (int k = 0; k < K; k++) {
-    //     cout << tDP[now][k][3] << " ";
-    // }
-    // cout << endl;
-    // getchar();
+bool canMake(int nowColor, int nxColor) {
+    if (nowColor == 1) return true;
+    else if (nowColor == 2) return nxColor != 2;
+    else return nowColor == 2;
 }
 
 void makeTree(int now, int parent) {
@@ -90,53 +30,22 @@ void makeTree(int now, int parent) {
 
 int main()
 {
-    fin >> V >> Q >> K;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> V >> Q >> K;
     for (int i = 1; i < V; i++) {
         int a, b;
-        fin >> a >> b;
+        cin >> a >> b;
         G[a].push_back(b);
         G[b].push_back(a);
     }
-    for (int i = 1; i <= V; i++) {
-        fin >> Cost[i][1] >> Cost[i][2] >> Cost[i][3];
+    for (int i = 1; i <= V; i++) { // R: 1, G: 2, B: 3
+        cin >> Cost[i][1] >> Cost[i][2] >> Cost[i][3];
     }
-    makeTree(1, 0);        
-    for (int i = 1; i <= 3; i++)
-        dfs(1, i, 0);
-
-    vector<int> Test = { 4 };
-    for (int t : Test) {
-        cout << t << endl;
-        cout << "R: " << endl;
-        for (int k = 0; k < K; k++) {
-            cout << k << ": "; 
-            for (int j = 0; j <= 3; j++)
-                cout << tDP[t][k][1][j] << " ";
-            cout << endl;
-        }
-        cout << endl;
-        cout << "G: " << endl;
-        for (int k = 0; k < K; k++) {
-            cout << k << ": "; 
-            for (int j = 0; j <= 3; j++)
-                cout << tDP[t][k][1][j] << " ";
-            cout << endl;
-        }
-        cout << endl;
-        cout << "B: " << endl;
-        for (int k = 0; k < K; k++) {
-            cout << k << ": "; 
-            for (int j = 0; j <= 3; j++)
-                cout << tDP[t][k][1][j] << " ";
-            cout << endl;
-        }
-        cout << endl << endl;
-        getchar();
-    }
+    makeTree(1, 0);
 
     int ans = 0;
-    for (int i = 1; i <= 3; i++)
-        ans = (ans + tDP[1][0][i][0]) % Q;
 
     cout << ans << "\n";
     return 0;
