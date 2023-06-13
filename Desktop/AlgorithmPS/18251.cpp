cd5d6937125f338be56e1ed6709bb20d118fd5f2
Michael <kimh060612@autocrypt.io>
2023-06-13 14:40:40 +0900

    .

--- Desktop/AlgorithmPS/18251.cpp
+++ Desktop/AlgorithmPS/18251.cpp
@@ -2,48 +2,46 @@
 #include <math.h>
 #include <vector>
 #include <algorithm>
-#define INF 9876543210
+#define INF 1e18
 
 using namespace std;
 using ll = long long;
 using llp = pair<ll, ll>;
 
 int N, H;
-ll T[300000];
-ll DP[300000][35];
-vector<llp> X, Y[35];
+ll T[662144];
+vector<llp> X;
 
 ll min(ll a, ll b) { return a < b ? a : b; }
 ll max(ll a, ll b) { return a > b ? a : b; }
 
 void dfs(int now, int depth) {
-    if (T[now] == INF) return;
     int left = 2 * now, right = 2 * now + 1;
-
-    dfs(left, depth + 1);
-    X.push_back({ now, depth });
-    Y[depth].push_back({ now, T[now] });
-    dfs(right, depth + 1);
+    if (left <= N) dfs(left, depth + 1);
+    X.push_back({ T[now], depth });
+    if (right <= N) dfs(right, depth + 1);
 }
 
 int main()
 {
     cin >> N;
     H = (int)ceil(log2(N + 1));
-    fill(T + 1, T + 262144, INF);
     for (int i = 1; i <= N; i++) {
         cin >> T[i];
     }
-
+    X.push_back({ 0, 0 });
     dfs(1, 1);
     
     ll ret = -INF;
-    DP[1][H] = T[X[0].first];
-    for (int i = 1; i < N; i++) {
-        int w = T[X[i].first];
-        int y = X[i].second;
-        for (int h = 1; h <= H; h++) {
-            
+    for(int i = 1; i <= H; i ++){
+        for(int j = i; j <= H; j++){
+            ll now = 0;
+            for(int k = 1; k <= N; k++){
+                if(X[k].second < i || X[k].second > j) continue;
+                now = now + X[k].first;
+                ret = max(ret, now);
+                if(now < 0) now=0;
+            }
         }
     }
     cout << ret << "\n";
