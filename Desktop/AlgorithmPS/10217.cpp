79680fb392481002c3ab4873046e178bf937f7bd
Michael <kimh060612@autocrypt.io>
2023-06-19 11:08:27 +0900

    .

--- Desktop/AlgorithmPS/10217.cpp
+++ Desktop/AlgorithmPS/10217.cpp
@@ -4,59 +4,35 @@
 #include<algorithm>
  
 using namespace std;
- 
+
+struct info {
+	int node;
+	int fee;
+	int dist;
+};
+
+// 우선순위큐 실행을 위한 커스텀 comparator
+class compare {
+public:
+	bool operator()(info& e1, info& e2) {
+		// compare d
+		return e1.fee > e2.fee;
+	}
+};
+
 //앞에는 다음 공항, 뒤는 cost
-vector<pair<int, pair<int, int> > > idx[101];
 const int INF = 1000000007;
+vector<info> idx[101];
 int timeDelay[101][10001];
- 
-void dijkstra(const int& N, const int& MAX_COST) {
-    for (int i = 0; i <= N; i++) {
-        for (int j = 0; j <= MAX_COST; j++)
-            timeDelay[i][j] = INF;
-    }
-    timeDelay[1][0] = 0;
-    priority_queue<pair<int, pair<int, int> > > pq;
-    pq.push({ 0, {0, 1} });
-    while (!pq.empty()) {
-        int cost = -pq.top().first;
-        int time = pq.top().second.first;
-        int here = pq.top().second.second;
-        pq.pop();
-        if (timeDelay[here][cost] < time)
-            continue;
-        for (int i = 0; i < idx[here].size(); i++) {
-            int nextCost = cost + idx[here][i].second.first;
-            int there = idx[here][i].first;
-            int nextTime = time + idx[here][i].second.second;
-            
-            //만일 최대 비용을 초과하는 가격이 나온다면, 패스한다.
-            if (nextCost > MAX_COST)
-                continue;
-            //이 비용 위로 이 시간이 최소 시간이 되는지 확인하고, 미리 설정해준다.
-            //이유는, pq에 push를 최소로 하도록 만들어주고, 이미 들어간 값들의 무의미한 계산을 막아주기 위해서이다.
-            if (timeDelay[there][nextCost] > nextTime) {
-                for (int i = nextCost; i <= MAX_COST; i++) {
-                    if (timeDelay[there][i] > nextTime)
-                        timeDelay[there][i] = nextTime;
-                }
-                pq.push({ -nextCost, {nextTime, there} });
-            }
-        }
-    }
-    int ret = INF;
-    for (int i = 1; i <= MAX_COST; i++)
-        ret = min(ret, timeDelay[N][i]);
-    if (ret == INF)
-        cout << "Poor KCM" << endl;
-    else
-        cout << ret << endl;
-}
+priority_queue<info, vector<info>, compare> pq;
+
+int min(int a, int b) { return a < b ? a : b; }
  
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
- 
+    cout.tie(0);
+
     //T=테스트 케이스 수, N=공항의 수, M=지원 비용, K=티켓정보의 수
     int T, N, M, K;
     //u=출발공항, v=도착공항, c=비용, d=소요시간
@@ -64,18 +40,44 @@ int main() {
     cin >> T;
     while (T--) {
         cin >> N >> M >> K;
-        for (int i = 1; i <= N; i++) {
-            while (!idx[i].empty())
-                idx[i].pop_back();
-        }
+        for (int i = 1; i <= N; i++) idx[i].clear();
         for (int i = 0; i < K; i++) {
             cin >> u >> v >> c >> d;
-            idx[u].push_back({ v, {c, d} });
+            idx[u].push_back({ v, c, d });
         }
-        dijkstra(N, M);
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= M; j++)
+                timeDelay[i][j] = INF;
+        }
+        timeDelay[1][0] = 0;
+        pq.push({ 1, 0, 0 });
+        while (!pq.empty()) {
+            int cost = pq.top().fee;
+            int time = pq.top().dist;
+            int here = pq.top().node;
+            pq.pop();
+            if (timeDelay[here][cost] < time) continue;
+            for (int i = 0; i < idx[here].size(); i++) {
+                int nextCost = cost + idx[here][i].fee;
+                int there = idx[here][i].node;
+                int nextTime = time + idx[here][i].dist;
+                if (nextCost > M) continue;
+                if (timeDelay[there][nextCost] <= nextTime) continue;
+
+                for (int j = nextCost; j <= M; j++) {
+                    if (timeDelay[there][j] > nextTime) timeDelay[there][j] = nextTime;
+                }
+                pq.push({ there, nextCost, nextTime });
+            }
+        }
+        int ret = INF;
+        for (int i = 1; i <= M; i++)
+            ret = min(ret, timeDelay[N][i]);
+        if (ret == INF)
+            printf("Poor KCM\n");
+        else
+            printf("%d\n", ret);
     }
-    
- 
- 
     return 0;
 }
