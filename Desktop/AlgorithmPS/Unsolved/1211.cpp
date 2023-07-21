4b19c7db1c9f0de155a14256ebf6f3c642de3490
michael <kimh060612@naver.com>
2023-07-21 23:53:03 +0900

    .

--- Desktop/AlgorithmPS/Unsolved/1211.cpp
+++ Desktop/AlgorithmPS/Unsolved/1211.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 using ll = long long;
 
+ll max(ll a, ll b) { return a > b ? a : b; }
+
 ll selfProduct(ll n) { // O(lg N)
     ll res = 1, tmp = n;
     while (tmp) {
@@ -17,10 +19,11 @@ ll selfProduct(ll n) { // O(lg N)
 int main()
 {
     ll lim;
-    ll index = 0, k;
+    ll index = 0, k, ret = -MAX;
     cin >> lim;
     for (int i = 1; i <= lim; i++) {
         k = selfProduct(i);
+        ret = max(ret, k);
         index = i;
         if (k > MAX) break;
     }
