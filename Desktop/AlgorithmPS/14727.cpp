bb80a51108cfde23c2138d249ae7c1348c148fe2
michael <kimh060612@naver.com>
2023-08-26 20:17:28 +0900

    .

---
+++ Desktop/AlgorithmPS/14727.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#define MAX 100005
+
+using namespace std;
+using ll = long long;
+
+ll N, A[MAX], tree[MAX * 5];
+
+ll max(ll a, ll b) { return a > b ? a : b; }
+
+void init(int node, int s, int e) {
+    if (s == e) tree[node] = s;
+    else {
+        int mid = (s + e) / 2;
+        init(node * 2, s, mid);
+        init(node * 2 + 1, mid + 1, e);
+        int left = A[tree[node * 2]], right = A[tree[node * 2 + 1]];
+        if (left <= right) tree[node] = tree[node * 2];
+        else tree[node] = tree[node * 2 + 1];
+    }
+}
+
+ll query(int node, int s, int e, int qs, int qe) {
+    if (s > qe || e < qs) return -1;
+    if (qs <= s && e <= qe) return tree[node];
+    int mid = (s + e) / 2;
+    ll left = query(node * 2, s, mid, qs, qe);
+    ll right = query(node * 2 + 1, mid + 1, e, qs, qe);
+    if (left == -1) return right;
+    else if (right == -1) return left;
+    else {
+        if (A[left] <= A[right]) return left;
+        else return right;
+    }
+}
+
+ll maxArea(int s, int e) {
+    ll gIndex = query(1, 1, N, s, e);
+    ll ret = A[gIndex] * (e - s + 1);
+    if (s <= gIndex - 1) {
+        ll x = maxArea(s, gIndex - 1);
+        ret = max(ret, x);
+    }
+    if (gIndex + 1 <= e) {
+        ll x = maxArea(gIndex + 1, e);
+        ret = max(ret, x);
+    }
+    return ret;
+}
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    
+    init(1, 1, N);
+    ll ret = maxArea(1, N);
+    cout << ret << "\n";
+    return 0;
+}
